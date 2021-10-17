21a6691563e0fd3a8ce511036ef5c9fa62aa6d8f
Ben <ben.atkinson.official@gmail.com>
2021-10-17 22:02:11 +1100

    feat: App heading and inclusion of Grid and Cell components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import CellGrid from "./CellGrid";
+
+const AppHeader = styled.h1`
+  text-align: center;
+`;
+const AppSubHeader = styled.h2`
+  text-align: center;
+`;
+
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <AppHeader>Cell Simulation</AppHeader>
+      <AppSubHeader>Belong Tech Test</AppSubHeader>
+      <CellGrid></CellGrid>
+    </div>
+  );
 };
 
 export default App;
